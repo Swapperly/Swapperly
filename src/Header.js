346148247f6183eaaa1f223b9838a4fbe7ac6ad3
vizireanuandrei-cristian <vizireanuandreicristian10@gmail.com>
2021-12-01 20:13:10 +0000

    new string test

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header() {
             </div> 
 
             <div className='header_right'>
-                <p>Become a host</p>
+                <p>Become a host. TEST</p>
                 <LanguageIcon />
                 <ExpandMoreIcon />
                 <Avatar />
