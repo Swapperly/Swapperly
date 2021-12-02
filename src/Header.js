dba1be43cc3a5bb4bb323d02d3d43d7c998b3422
vizireanuandrei-cristian <vizireanuandreicristian10@gmail.com>
2021-12-02 16:58:56 +0000

    changed back the string

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header() {
             </div> 
 
             <div className='header_right'>
-                <p>Become a host. TEST2222</p>
+                <p>Become a host.</p>
                 <LanguageIcon />
                 <ExpandMoreIcon />
                 <Avatar />
