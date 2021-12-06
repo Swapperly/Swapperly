77ee427d2168f26146f77500dbb97ed44fe97a2e
vizireanuandrei-cristian <vizireanuandreicristian10@gmail.com>
2021-12-06 21:07:32 +0000

    test deployed 3

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header() {
             </div> 
 
             <div className='header_right'>
-                <p>Become a host. Deployed test 2</p>
+                <p>Become a host. Deployed test 3</p>
                 <LanguageIcon />
                 <ExpandMoreIcon />
                 <Avatar />
